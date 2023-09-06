bdb6e249ace0f2a75b6831bb7a1ec0292802e15b
Arpan Das <arpan.das@klythe.com>
2023-09-06 16:44:55 +0530

    init

--- Routes/Todolist.js
+++ Routes/Todolist.js
@@ -1,17 +1,20 @@
-import express from 'express'
+import express from "express";
 
-import { getAllTodos } from '../Controller/controller.js';
-import { addTodo,toggleTodoDone,updatedTodos,deleteTodo } from '../Controller/controller.js';
-const route= express.Router();
-route.post("/todos",addTodo)
-route.get("/getTodos",getAllTodos)
-route.put('/todos/:id', toggleTodoDone);
-route.put('/todosUpdate/:id',updatedTodos)
-route.delete('/todosDelete/:id',deleteTodo)
-route.get('/',(req,res)=>{
-res.send("<h1>Todo List</h1>")
-})
-  
-
+import { getAllTodos } from "../Controller/controller.js";
+import {
+  addTodo,
+  toggleTodoDone,
+  updatedTodos,
+  deleteTodo,
+} from "../Controller/controller.js";
+const route = express.Router();
+route.post("/todos", addTodo);
+route.get("/getTodos", getAllTodos);
+route.put("/todos/:id", toggleTodoDone);
+route.put("/todosUpdate/:id", updatedTodos);
+route.delete("/todosDelete/:id", deleteTodo);
+route.get("/", (req, res) => {
+  res.send("<h1>Todo List</h1>");
+});
 
 export default route;
